e6339bf4e30dc66f08f635c271f3b0354007ad83
El-Dringo-Brannde <Brandon.d.junk@gmail.com>
2017-04-15 09:51:34 -0700

    Can now spawn pages and stack them,
    When spawning pages it stacks to the card at the spawn point in one position

--- js/cards.js
+++ js/cards.js
@@ -13,7 +13,6 @@ function Card(id) {
     header.innerHTML = "id:" + this.id;
     header.onmousedown = function() {
         var highestZIndex = getHighestZIndexCard();
-        console.log(highestZIndex);
         header.style.zIndex = ++highestZIndex;
         div.style.zIndex = ++highestZIndex;
     }
@@ -23,7 +22,7 @@ function Card(id) {
     close_button.setAttribute("value", "x");
     close_button.innerHTML = "x";
     close_button.onclick = function() {
-        $("#" + id).remove(); // remove first element with 'id' tag
+        $("#" + id).remove(); // remove element with specified 'id' tag
     };
 
     var editor = document.createElement('textarea');
@@ -37,34 +36,78 @@ function Card(id) {
     div.appendChild(header);
     div.appendChild(editor);
     setPosition(div);
-    document.body.appendChild(div);
+
+
+
     $(".card").draggable({
         handle: ".card-header"
+    });
+    var children = 0;
+    $(".container").droppable({
+        drop: function(event, ui) {
+            $(this).append($(ui.draggable));
+            $(ui.draggable).css({
+                top: event.pageY,
+                left: event.pageX
+            });
+        }
+    });
+
+    $(".card").droppable({
+        drop: function(event, ui) {
+            children = $(this).children().size();
+            if (children - 1 != 0)
+                children--;
+            $(this).append($(ui.draggable)); //appends it to inside the div
+            $(ui.draggable).css({ //repositions child div
+                top: 40 * (children), // - 1 for textBox inside div
+                left: 15 * (children)
+            });
+        },
+        out: function(event, ui) {
+            $(".container").append($(ui.draggable)).css({
+                top: event.pageY,
+                left: event.pageX
+            });
+        }
     });
 }
 
 // Gets the highest z Index so that the clicked card will appear at the front.
 function getHighestZIndexCard() {
     var cards = document.getElementsByClassName("card");
-    console.log(cards);
     var highestSoFar = 0;
     for (var i = 0; i < cards.length; i++) {
         if (parseInt(cards[i].style.zIndex) > highestSoFar) {
             highestSoFar = parseInt(cards[i].style.zIndex);
         }
     }
-    console.log("This is the highest z index", highestSoFar);
     return highestSoFar;
 }
 
 //To make sure that when a card is created it stacks on top of the other cards
 function setPosition(div) {
-    var cards = document.getElementsByClassName("card");
+    let cards = document.getElementsByClassName("card");
+    let zVal = getHighestZIndexCard();
+    var eleAtStart = {};
+    let multiplier = 0;
     div.style.position = "absolute";
-    if (cards.length != 0) {
-        var zVal = getHighestZIndexCard();
-        div.style.zIndex = ++zVal;
-        div.style.left = (cards[cards.length - 2].offsetLeft + 6) + "px";
-        div.style.top = (cards[cards.length - 2].offsetTop + 35) + "px";
+    div.style.zIndex = ++zVal; // the newest window will be on top of the stack
+    if (cards.length == 0) { // if there are no cards on page
+        document.body.appendChild(div);
+        return;
     }
+    for (var i = 0; i < cards.length; i += 2) { // +=2 to only get the cards not the headers
+        if (cards[i].style.left == "" && cards[i].style.top == "") {
+            eleAtStart = cards[i]; // get whoever is at the start at spawn point
+            $("#" + eleAtStart.id).append(div);
+            multiplier = eleAtStart.getElementsByTagName("div").length; //for offset 
+            $("#" + div.id).css({
+                top: 20 * (multiplier - 1),
+                left: 5 * (multiplier - 1)
+            });
+            return;
+        }
+    }
+    document.body.appendChild(div);
 }
