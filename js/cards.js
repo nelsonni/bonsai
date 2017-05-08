958d19b1cd8e49b13a2d04384a6352968b273fc2
Brandon Dring <Brandon.d.junk@gmail.com>
2017-05-08 12:10:47 -0700

    Card expansion works, high light box scales, and buttons can be added dynamically with expansion

--- js/cards.js
+++ js/cards.js
@@ -23,6 +23,9 @@ function Card(id) {
     close_button.setAttribute("class", "close");
     close_button.setAttribute("value", "x");
     close_button.innerHTML = "x";
+    close_button.onclick = function () {
+        closeCard(id)
+    };
     header.appendChild(close_button);
 
     var editor = document.createElement('textarea');
@@ -54,7 +57,7 @@ function Card(id) {
     $(flipBtn).css({
         position: "fixed",
         top: 270,
-        left: 190,
+        left: 190
     });
     flipBtn.onclick = function () {
         handleCardFlip(div, flipBtn);
@@ -159,47 +162,48 @@ function toggleFullscreen(div, btn) {
 
 
 function cardExpansion(id, btn) {
-    var test = $("#" + id);
     btn.innerHTML = "Close";
-    var base = getBottomStack(test, test);
-    base = $("#" + base.id);
-    $(base[0].children).each(function (idx) {
-        if (this.classList.contains("Test")) {
-            $(this.firstChild).click(function () { // some work can be done regarding editing cards while expanded
-                document.body.append(this.parentNode);
-            });
+    var base = $("#" + id);
+    $(base[0].children).each(function (idx) { // possible bug since took away the click listener?
+        if (this.classList.contains("actualCard")) {
+            addButtonsBack(this);
             $(this).css({
-                top: base[0].style.top,
+                top: parseInt(base[0].style.top) + 10,
                 left: parseInt(base[0].style.left) + (225 * (idx))
             });
         }
     });
+    var newWidth = parseInt(base[0].style.width) +
+        (parseInt(base[0].lastChild.style.left) + 15);
+    $(base)
+        .css({width: newWidth})
+        .addClass("expanded");
+    $(btn).css({left: newWidth});
     btn.onclick = function () {
-        collapseCards(id, base);
-        $("#collapseBtn" + id).remove();
-        btn.innerHTML = "Expnd";
-        btn.onclick = function () {
-            cardExpansion(id, btn);
-        }
+        collapseCards(btn, base);
     };
 }
 
 
-function collapseCards(id, base) {
+function collapseCards(btn, base) {
+    $(base)
+        .css({width: 200 + ((base.children.length) * 10)})
+        .removeClass("expanded");
+    $(btn).css({left: 200 + ((base.children.length) * 10)});
     $(base[0].children).each(function (idx) {
-        if (this.classList.contains("Test")) {
-            if (idx > 1) {
-                $(this).css({
-                    top: parseInt(base[0].style.top) + (20 * (idx)),
-                    left: parseInt(base[0].style.left) + (5 * (idx))
-                });
-            } else
-                $(this).css({
-                    top: parseInt(base[0].style.top) + (20 * (idx)),
-                    left: parseInt(base[0].style.left) + (5 * (idx))
-                });
+        if (this.classList.contains("actualCard")) {
+            $(this).css({
+                top: parseInt(base[0].firstElementChild.style.top) + (20 * (idx)),
+                left: parseInt(base[0].style.left) + (5 * (idx + 1))
+            });
         }
     });
+
+    btn.innerHTML = "Expnd";
+    btn.onclick = function () {
+        cardExpansion(id, btn);
+    };
+    hideButtons(base[0]);
 
 }
 
@@ -240,7 +244,7 @@ function moveStackEffects(latestAdd, base) {
         id = base.parentNode;
     hideButtons(id);
     $(id.children).each(function () {
-        if (this.classList.contains("Test")) {
+        if (this.classList.contains("actualCard")) {
             console.log(this);
             //$(this).draggable("destroy");
             $(this.firstElementChild).draggable({
@@ -261,7 +265,7 @@ function moveStackEffects(latestAdd, base) {
                     });
                     $(document.getElementsByClassName("expandableBtn")).remove();
                     $(id.children).each(function () {
-                        if (this.classList.contains("Test") &&
+                        if (this.classList.contains("actualCard") &&
                             $(this)[0].nextElementSibling !== null) {
                             console.log($(this));
                             var top = parseFloat($(this)[0].nextElementSibling.style.top) - 20;
@@ -275,7 +279,7 @@ function moveStackEffects(latestAdd, base) {
                         left: parseFloat(id.firstElementChild.style.left) - 5
                     });
                 },
-                start: function (event, ui) {
+                start: function () {
                     $('.card').toggleClass('notransition');
                 },
                 stop: function () {
@@ -304,24 +308,33 @@ function setHighlightBox(base, curCard) {
             hoverClass: "ui-state-highlight"
         }).hover(// on highlight box mouseover
             function () { // changes background of hover and creates the expand btn on hover
+                if (document.getElementsByClassName("expandableBtn").length !== 0)
+                    return;
                 $(this).css("background", "rgba(0, 246, 255, 0.20)");
                 var expandBtn = document.createElement("button");
                 expandBtn.setAttribute("class", "expandableBtn");
                 expandBtn.setAttribute("id", "expandableBtn");
-                expandBtn.innerHTML = "Expnd";
                 box.append(expandBtn);
                 $("#expandableBtn").css({
                     position: "fixed",
                     top: parseInt(this.style.height) + parseInt(this.style.top) - 12,
                     left: parseInt(this.style.width) + parseInt(this.style.left) - 35
                 });
-                expandBtn.onclick = function () {
-                    cardExpansion(box.id, this);
+                if (!box.classList.contains("expanded")) {
+                    expandBtn.innerHTML = "Expnd";
+                    expandBtn.onclick = function () {
+                        cardExpansion(box.id, this);
+                    }
+                } else {
+                    expandBtn.innerHTML = "Close";
+                    expandBtn.onclick = function () {
+                        collapseCards(expandBtn, $(box));
+                    };
                 }
             }, // on mouse out
             function () {
                 $(this).prop("style").removeProperty("background");
-                $("#expandableBtn").remove();
+                $(".expandableBtn").remove();
             }
         );
         $(box).append(base);
@@ -336,20 +349,16 @@ function setHighlightBox(base, curCard) {
             });
         else {
             $(box).append(curCard[0]);
-            var isDragging = false;
             var firstHeader = base.firstElementChild.firstElementChild;
             setClickEffects(curCard[0], box, base);
             firstHeader.onmousedown = function () {
                 cardPreview(base, firstHeader, box, base);
             };
         }
-
-
-
-    } else { // If there is already a box there
+    } else { // If there is already a highlightBox
         var box = document.getElementById(base.id);
         document.body.appendChild(curCard[0]);
-        $(box).css({ //move the highlight box
+        $(box).css({ //grow the highlight box
             height: parseInt($(box)[0].style.height) + 15,
             width: parseInt($(box)[0].style.width) + 10
         });
@@ -364,12 +373,17 @@ function setHighlightBox(base, curCard) {
 
 function cardPreview(cur, firstHeader, box, base) {
     var isDragging = false;
-    if (!$(cur)[0].parentNode.classList.contains("container") && !$(cur)[0].classList.contains("ui-draggable"))
-        $(cur.firstElementChild).draggable("destroy");
+    try {
+        if (!$(cur)[0].parentNode.classList.contains("container"))
+            $(cur.firstElementChild).draggable("destroy"); // remove the handle on the editor
+    } catch (err) {
+        console.log("Tried");
+    }
     $(cur).draggable({
         handle: ".card-header",
         containment: "window",
         start: function (event, ui) {
+            cur.style.zIndex = getHighestZIndexCard();
             $('.card').toggleClass('notransition');
             if (!cur.parentNode.classList.contains("container"))
                 breakOutOfBox(cur, box, base);
@@ -382,6 +396,10 @@ function cardPreview(cur, firstHeader, box, base) {
     firstHeader.onmouseup = function () { // handle just click events.
         var prevZIndex = cur.style.zIndex;
         if (!isDragging) {
+            if (event.target.classList.contains("close")) {
+                closeCard(event.target.id);
+                return;
+            }
             console.log("Just a click", cur);
             cur.style.zIndex = getHighestZIndexCard();
             $(cur).mouseleave(function () {
@@ -403,17 +421,15 @@ function shrinkBox(box) {
 
 function closeCard(id) {
     var card = $("#" + id)[0].parentNode.parentNode.parentNode;
-    console.log(card);
-    //When it is just the single card pulled out of stack
-    if (!card.parentNode.classList.contains("highlightBox"))
-        $(card).remove();
-    else {
-        alert("Can't delete card while in stack.");
+    try { // when it's just a single card that hasn't been added to a stack
+        if (card === document) throw "Bad base";
+    } catch (err) {
+        if (err === "Bad base") card = $("#" + id)[0];
     }
-
+    $(card).remove();
 }
 
-function addButtonsBack(curCard) {
+function addButtonsBack(curCard) { //show all buttons out of stack
     $($(curCard).find("button")).each(function () {
         $(this).show();
     });
@@ -422,10 +438,6 @@ function addButtonsBack(curCard) {
 
 function breakOutOfBox(cur, box, base) {
     console.log(cur);
-    if (event.target.classList.contains("close")) {
-        closeCard(event.target.id);
-        return;
-    }
     shrinkBox(box);
     cur.style.zIndex = getHighestZIndexCard();
     //$(cur).draggable("destroy");
@@ -434,17 +446,15 @@ function breakOutOfBox(cur, box, base) {
     if (isBottom($(cur), base) === false) // if the card is not the bottom card rearrange them
         arrangeLowerCards($(cur), $(cur.parentNode));
     document.body.appendChild(cur);
+    var baseHead = box.firstElementChild.firstElementChild;
     if ($(box).children("div").length === 1) {
-        console.log($(cur));
-
-        $(base.firstElementChild).draggable("destroy");
+        if (baseHead.classList.contains("ui-draggable"))
+            $(baseHead).draggable("destroy");
         $(base).draggable({
             handle: ".card-header",
             containment: "window",
             start: function (event, ui) {
                 $('.card').toggleClass('notransition');
-                console.log("here");
-                base.style.zIndex = getHighestZIndexCard();
             },
             stop: function (event, ui) {
                 $('.card').toggleClass('notransition');
@@ -457,18 +467,18 @@ function breakOutOfBox(cur, box, base) {
 }
 
 function setClickEffects(cur, box, base) {
-    if (cur.classList.contains("Test")) {
+    if (cur.classList.contains("actualCard")) {
         $(cur).css({
             top: parseInt(cur.previousElementSibling.style.top) + 20,
             left: parseInt(cur.previousElementSibling.style.left) + 5
         });
         $(box).append(cur);
-        // when someone clicks on the header of a card
         var firstHeader = cur.firstElementChild.firstElementChild;
         if (firstHeader.classList.contains("close")) //hack for 2 card stacks
             firstHeader = firstHeader.parentNode;
+        // when someone clicks on the header of a card
         firstHeader.onmousedown = function () {
-            isDragging = cardPreview(cur, firstHeader, box, base)
+            isDragging = cardPreview(cur, firstHeader, box, base);
         };
     }
 }
@@ -476,7 +486,7 @@ function setClickEffects(cur, box, base) {
 function mergeStacks(curCard) {
     var base = curCard[0].parentNode.firstElementChild;
     $(curCard[0].parentNode.children).each(function () {
-        if (this !== base && this.classList.contains("Test"))
+        if (this !== base && this.classList.contains("actualCard"))
             $(base).append(this);
     });
     document.body.appendChild(base);
@@ -527,47 +537,20 @@ function getHighestZIndexCard() {
 
 //To make sure that when a card is created it stacks on top of the other cards
 function setDivPosition(div) {
-    var cards = document.getElementsByClassName("card");
-    var eleAtStart = {};
-    var multiplier = 0;
+    var cards = document.getElementsByClassName("actualCard");
     div.style.position = "fixed";
     div.style.zIndex = getHighestZIndexCard(); // the newest window will be on top of the stack
     div.style.top = "35px";
     div.style.left = "10px";
-    div.className += " Test";
-    if (cards.length === 0) { // if there are no cards on page
-        document.body.appendChild(div);
-        $("#" + div.id).mousedown(function (event) {
-            if (event.target.parentNode.classList.contains("Base")) {
-                $(div).children('.card').each(function () {
-                    if (!this.classList.contains("card-header"))
-                        document.body.appendChild(this); // used to bring cards to front
-                });
-            }
-        });
-        console.log($(div));
-        div.firstElementChild.firstElementChild.onmousedown = function () {
-            div.style.zIndex = getHighestZIndexCard();
-        };
-        return;
-    } else {
-        //    alert("Edit and move already spawned card first.");
-        //return;
-    }
+    div.className += " actualCard";
 
-    // Jesus christ this is ugly, refactor this at some point
-    for (var i = 0; i < cards.length; i += 2) { // +=2 to only get the cards not the headers
-        if (cards[i].style.left == "10px" && cards[i].style.top == "35px") {
-            eleAtStart = cards[i]; // get whoever is at the start at spawn point
-            document.body.append(div);
-            multiplier = eleAtStart.getElementsByTagName("div").length; //for offset
-            $("#" + div.id).css({
-                top: 35 + (20 * (multiplier - 1)),
-                left: 10 + (5 * (multiplier - 1))
-            });
-            return; // prevent from being attached to doc.body
-        }
-    }
-    div.className += " Base";
+    var mult = 0; // searches for cards that have a direct position related to spawn point and calculates offset
+    for (var i = 0; i < cards.length; i++)
+        if (cards[i].style.top == ((35 + (30 * (i))).toString() + "px"))
+            mult += 1;
+    $(div).css({
+        top: ((35 + (30 * (mult))).toString() + "px"),
+        left: ((10 + (5 * mult)).toString() + "px")
+    });
     document.body.appendChild(div);
 }
--- main.js
+++ main.js
@@ -32,8 +32,9 @@ function createWindow() {
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         win = null
-    };
+    }
 )
+    ;
 }
 
 // This method will be called when Electron has finished
@@ -45,7 +46,7 @@ app.on('ready', createWindow);
 app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin';
+    if (process.platform !== 'darwin'
 )
 {
         app.quit()
@@ -55,7 +56,7 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (win === null;
+    if (win === null
 )
 {
     createWindow();
