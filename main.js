6b88624badaeff33185438693ce82d421c7d8abe
Brandon Lee Dring <Brandon.d.junk@gmail.com>
2017-06-30 08:12:10 -0700

    Multithreading for electron working

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
-const {
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+if (cluster.isMaster) {
+  const {
     app,
     BrowserWindow
-} = require('electron');
-const path = require('path');
-const url = require('url');
+  } = require('electron');
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win;
+  const path = require('path');
+  const url = require('url');
 
-function createWindow() {
+  // Keep a global reference of the window object, if you don't, the window will
+  // be closed automatically when the JavaScript object is garbage collected.
+  let win;
+
+  function createWindow() {
     // Create the browser window.
     win = new BrowserWindow({
-        width: 1200,
-        height: 1000
+      width: 1200,
+      height: 1000
     });
 
     // and load the index.html of the app.
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
     }));
 
     // Open the DevTools.
@@ -28,39 +32,50 @@ function createWindow() {
 
     // Emitted when the window is closed.
     win.on('closed', () => {
-            // Dereference the window object, usually you would store windows
-            // in an array if your app supports multi windows, this is the time
-            // when you should delete the corresponding element.
-            win = null
-        }
-    )
-    ;
-}
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      win = null
+    });
+  }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.on('ready', createWindow);
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
+  // Quit when all windows are closed.
+  app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin'
-    ) {
-        app.quit()
+    if (process.platform !== 'darwin') {
+      app.quit()
     }
-})
+  })
 
-app.on('activate', () => {
+  app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (win === null
-    ) {
-        createWindow();
+    if (win === null) {
+      createWindow();
     }
-})
+  })
 
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+  // In this file you can include the rest of your app's specific main process
+  // code. You can also put them in separate files and require them here.
+
+}
+
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+} else {
+  console.log("Alive and well")
+  console.log(`Worker ${process.pid} started`);
+}
