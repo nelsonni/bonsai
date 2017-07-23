d8054443967ca485e69f87dad6fa22d2c17e87c5
Brandon Lee Dring <Brandon.d.junk@gmail.com>
2017-07-23 09:37:06 -0700

    Button created upon selection, and captures selected text

--- js/codeEditors.js
+++ js/codeEditors.js
@@ -1,95 +1,104 @@
 require('./libs/ace/ext-modelist.js'); // Don't delete me! Needed by ace.req
 class CodeEditor extends Card {
-  constructor(type, fileData) {
-    super(type, fileData);
+   constructor(type, fileData) {
+      super(type, fileData);
 
-    this.type = type;
-    this.editors = [];
-    this.contentBuilder(this.card);
-    this.buildMetadata('codeEditor');
-  }
+      this.type = type;
+      this.editors = [];
+      this.contentBuilder(this.card);
+      this.buildMetadata('codeEditor');
+   }
 
-  // since the fullscreen class doesn't work on the ace_editor manually resize
-  toggleAceFullscreen(h, w) {
-    $(this.card).find('.ace_editor').each((idx, ele) => {
-      $(ele).css({
-        height: h,
-        width: w,
-      }); // resizes the code editor so everything scales properly.
-      $(ele).click();
-      this.editors.forEach((e, i) => e.resize());
-    });
-  }
-
-  ipcListeners() {
-    __IPC.remote.ipcMain.on('card' + this.id + '_toggle_fullscreen', (event, args) => {
-      this.toggleAceFullscreen(args[0], args[1]);
-    });
-  }
-
-  contentBuilder(card) {
-    var content = document.createElement('div');
-    $(content).attr({
-      class: 'editor',
-      id: card.id + '_editor_' + this.id,
-    });
-    let faces = [];
-    for (let i = 0; i < 3; i++) {
-      let face = document.createElement('div');
-      if (i == 2)
-        var faceEditor = document.createElement('div');
-      else
-        var faceEditor = document.createElement('textarea');
-      face.appendChild(faceEditor);
-      faces.push(face);
-    }
-
-    faces.forEach(function(element, idx) {
-      $(element.firstChild).attr({
-        class: 'editor',
-        id: card.id + 'codeEditor_' + idx,
+   // since the fullscreen class doesn't work on the ace_editor manually resize
+   toggleAceFullscreen(h, w) {
+      $(this.card).find('.ace_editor').each((idx, ele) => {
+         $(ele).css({
+            height: h,
+            width: w,
+         }); // resizes the code editor so everything scales properly.
+         $(ele).click();
+         this.editors.forEach((e, i) => e.resize());
       });
-      content.appendChild(element);
-    });
+   }
 
-    $(content).slick({
-      dots: true,
-      accessiblity: true,
-      focusOnSelect: true,
-      infinite: false,
-      edgeFriction: true,
-    });
-    this.carousel = content
-    $(content).find(".slick-arrow").hide()
-    $(content).find(".slick-dots").hide()
-    card.appendChild(content);
+   ipcListeners() {
+      __IPC.remote.ipcMain.on('card' + this.id + '_toggle_fullscreen', (event, args) => {
+         this.toggleAceFullscreen(args[0], args[1]);
+      });
+   }
 
-    // leave out last card so it can be used for metadata
-    this.initAce(faces.slice(0, faces.length - 1));
-  }
-
-  sendSave(idx) {
-    ipcRenderer.send('saveSignal', {
-      data: this.editors[idx].getValue(),
-      fileName: this.name,
-      location: this.location
-    });
-    $('body').addClass('waiting');
-  }
-
-  initAce(faces) {
-    let cur = this;
-    $(faces).each(function(idx) {
-      let editor = ace.edit(this.lastElementChild.id);
-      editor.setTheme('ace/theme/twilight');
-      var modelist = ace.require('ace/ext/modelist');
-      if (cur.fileExt != undefined) {
-        var mode = modelist.getModeForPath(cur.fileExt).mode;
-        editor.session.setMode(mode);
+   contentBuilder(card) {
+      var content = document.createElement('div');
+      $(content).attr({
+         class: 'editor',
+         id: card.id + '_editor_' + this.id,
+      });
+      let faces = [];
+      for (let i = 0; i < 3; i++) {
+         let face = document.createElement('div');
+         if (i == 2)
+            var faceEditor = document.createElement('div');
+         else
+            var faceEditor = document.createElement('textarea');
+         face.appendChild(faceEditor);
+         faces.push(face);
       }
 
-      editor.on('change', () => cur.updateMetadata('codeEditor'));
-      cur.editors.push(editor);
-    });
-  }
+      faces.forEach(function(element, idx) {
+         $(element.firstChild).attr({
+            class: 'editor',
+            id: card.id + 'codeEditor_' + idx,
+         });
+         content.appendChild(element);
+      });
+
+      $(content).slick({
+         dots: true,
+         accessiblity: true,
+         focusOnSelect: true,
+         infinite: false,
+         edgeFriction: true,
+      });
+      this.carousel = content
+      $(content).find(".slick-arrow").hide()
+      $(content).find(".slick-dots").hide()
+      card.appendChild(content);
+
+      // leave out last card so it can be used for metadata
+      this.initAce(faces.slice(0, faces.length - 1));
+   }
+
+   sendSave(idx) {
+      ipcRenderer.send('saveSignal', {
+         data: this.editors[idx].getValue(),
+         fileName: this.name,
+         location: this.location
+      });
+      $('body').addClass('waiting');
+   }
+
+   copyText(editor) {
+      console.log(editor.getCopyText(), this, canvas)
+      let foo = document.createElement("button")
+      $(foo).html("Export")
+      $(this.card).append(foo)
+
+   }
+
+   initAce(faces) {
+      let cur = this;
+      $(faces).each(function(idx) {
+         let editor = ace.edit(this.lastElementChild.id);
+         editor.setTheme('ace/theme/twilight');
+         var modelist = ace.require('ace/ext/modelist');
+         if (cur.fileExt != undefined) {
+            var mode = modelist.getModeForPath(cur.fileExt).mode;
+            editor.session.setMode(mode);
+         }
+
+         editor.on('change', () => cur.updateMetadata('codeEditor'));
+         $(editor).click(() => editor.getCopyText() == "" ? console.log("foo") : cur.copyText(editor))
+         cur.editors.push(editor);
+      });
+   }
 }
